8d91e2ad55a69b92e960ecff9fbd88b6da793b45
dextersjw <dextersjw@gmail.com>
2024-06-29 12:38:18 +0800

    created react app and initial commit

---
+++ guess-number-game-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GuessNumberGame from './components/GuessNumberGame';
+
+function App() {
+  return (
+    <div className="App">
+      <GuessNumberGame />
+    </div>
+  );
+}
+
+export default App;
